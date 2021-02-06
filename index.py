62c7c1d1076b88ff8e8533da80bb2f6c9617018e
Leo Gaunt <leogaunt05@outlook.com>
2021-02-06 17:05:07 +0000

    Added pyfiglet and variables

--- index.py
+++ index.py
@@ -1,11 +1,26 @@
+import json
 import pyfiglet
+from os import system
 import requests
 import re
 
 #Weather API - weather stack
 
+name = ''
+temperature = 0
+feelsLike = 0
+weatherDesc = ''
+
 def setup():
     name = input('What is your name?')
+    data = {}
+    data['info'] = []
+    data['info'].append({
+        'name': name
+    })
+    with open('info.json','w') as infofile:
+        json.dump(data, infofile)
+
 def getIPWeather():
     ip = requests.get('http://ip.42.pl/raw').text
     response = requests.get('http://ip-api.com/json/'+ip).json()
@@ -13,6 +28,9 @@ def getIPWeather():
     city = str(city).replace(" ", "%20")
     weather = requests.get('http://api.weatherstack.com/current?access_key=ef8b498ebf38e85990c49bcb3d4fc3b7&query='+city).json()
     print(weather)
+    temperature = weather['current']['temperature']
+    feelsLike = weather['current']['feelslike']
+    weatherDesc = weather['current']['weather_descriptions']
     
 with open('info.json') as f:
     if f.read() == '':
@@ -20,3 +38,6 @@ with open('info.json') as f:
         getIPWeather()
     else:
         getIPWeather()
+
+system('clear')
+ascii_banner = pyfiglet.figlet_format("Hello "+ name)
